e972e749097325be824c4a7087a6d8466f60d9bd
habray-99 <vamsha.tamu.a21.2@icp.edu.np>
2024-04-05 16:21:44 +0545

    Update DEBUG and ALLOWED_HOSTS settings

--- myproject/settings.py
+++ myproject/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-e@y4o3ufjtg!w&7zo%%u(9%p@_r-lfwnt_)r-^$2a%emyqqi42'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
