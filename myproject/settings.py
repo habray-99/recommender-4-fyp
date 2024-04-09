dc821ecc4189f4104d7fd7cceef55434f4a73fde
habray-99 <vamsha.tamu.a21.2@icp.edu.np>
2024-04-09 09:57:52 +0545

    Add CORS settings to allow localhost and specific methods

--- myproject/settings.py
+++ myproject/settings.py
@@ -27,7 +27,14 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:*",
+    "http://127.0.0.1:*",
+    "http://localhost:5554",
+    "http://127.0.0.1:5554",
+]
 
+CORS_ALLOW_METHODS =['get', 'GET']
 # Application definition
 
 INSTALLED_APPS = [
@@ -37,9 +44,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
